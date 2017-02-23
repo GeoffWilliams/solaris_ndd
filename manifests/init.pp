73d2c10bc4bd3398cdca6b9a65701c02c82b566b
Geoff Williams <geoff.williams@puppetlabs.com>
2017-02-24 03:13:19 +1100

    support for array-based values

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,11 @@
 #
 # @param value Value to set the parameter to (interpolated into string)
 # @param key Item to change in format DRIVER->PARAMETER, eg
-#   /dev/ip->ip_forward_src_routed, defaults to $title
+#   /dev/ip->ip_forward_src_routed, defaults to $title.  This is the writable
+#   form of the key name, eg tcp_extra_priv_ports_add if we need to ensure a
+#   port is added or tcp_extra_priv_ports_del if we need to ensure a port is
+#   removed, not just tcp_extra_priv_ports.  We will convert to this short name
+#   ourself in order to test the current settings, however
 define solaris_ndd(
   $value,
   $key = $title,
@@ -13,9 +17,13 @@ define solaris_ndd(
   if $key.match(/.+->.+/) {
     $key_split = split($key, '->')
 
+    # remove any _add or _del to check array data keys, we should still keep
+    # these to write though
+    $read_key = regsubst($key_split[1], /(_add|_del)/, "", "G")
+
     exec { "ndd ${key}":
       command => "ndd -set ${key_split[0]} ${key_split[1]} ${value}",
-      unless  => "ndd -get ${key_split[0]} ${key_split[1]} | grep ${value}",
+      unless  => "ndd -get ${key_split[0]} ${read_key} | grep ${value}",
       path    => [ "/usr/bin", "/usr/sbin", "/bin", "/sbin"],
     }
   } else {
